59f243ca9212702ade3de7fc2d943173aa2d9b04
hoangdang <nghiemdanghoang>
2022-01-12 22:50:10 +0700

    fix detailPage

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -38,29 +38,11 @@ const DetailPage = {
             </ol>
           </nav>
       
-          <!-- Image gallery -->
-          <div class="mt-6 max-w-2xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
-            <div class="hidden aspect-w-3 aspect-h-4 rounded-lg overflow-hidden lg:block">
-              <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg" alt="Two each of gray, white, and black shirts laying flat." class="w-full h-full object-center object-cover">
-            </div>
-            <div class="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
-              <div class="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
-                <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg" alt="Model wearing plain black basic tee." class="w-full h-full object-center object-cover">
-              </div>
-              <div class="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
-                <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg" alt="Model wearing plain gray basic tee." class="w-full h-full object-center object-cover">
-              </div>
-            </div>
-            <div class="aspect-w-4 aspect-h-5 sm:rounded-lg sm:overflow-hidden lg:aspect-w-3 lg:aspect-h-4">
-              <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg" alt="Model wearing plain white basic tee." class="w-full h-full object-center object-cover">
-            </div>
-          </div>
-      
           <!-- Product info -->
           <div class="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
             <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
               <h1 class="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
-                Basic Tee 6-Pack
+                ${findProduct.name}
               </h1>
             </div>
       
@@ -321,6 +303,24 @@ const DetailPage = {
                   <p class="text-sm text-gray-600">The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming &quot;Charcoal Gray&quot; limited release.</p>
                 </div>
               </div>
+            </div>
+          </div>
+
+          <!-- Image gallery -->
+          <div class="mt-6 max-w-2xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
+            <div class="hidden aspect-w-3 aspect-h-4 rounded-lg overflow-hidden lg:block">
+              <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg" alt="Two each of gray, white, and black shirts laying flat." class="w-full h-full object-center object-cover">
+            </div>
+            <div class="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
+              <div class="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
+                <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg" alt="Model wearing plain black basic tee." class="w-full h-full object-center object-cover">
+              </div>
+              <div class="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
+                <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg" alt="Model wearing plain gray basic tee." class="w-full h-full object-center object-cover">
+              </div>
+            </div>
+            <div class="aspect-w-4 aspect-h-5 sm:rounded-lg sm:overflow-hidden lg:aspect-w-3 lg:aspect-h-4">
+              <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg" alt="Model wearing plain white basic tee." class="w-full h-full object-center object-cover">
             </div>
           </div>
         </div>
