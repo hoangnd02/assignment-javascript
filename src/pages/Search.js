fa104d38ea29d62de628cebfb7f6a6692d3b252b
hoangdang <nghiemdanghoang>
2022-02-20 10:59:57 +0700

    fix css search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -33,7 +33,7 @@ const Search = {
 						<h2 class="text-2xl border-b-[1px] mx-[-16px] px-4 py-4 font-bold text-gray-900">
 						Search:  ${urlParams.get("q")}
 						</h2>
-						<div id="product_filter" class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+						<div id="product_filter" class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
 							${productList.print(data)}
 						</div>
 				</div>
