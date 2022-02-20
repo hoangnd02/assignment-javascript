3ff8943b529d5d7654770c8de4177449e104d266
hoangdang <nghiemdanghoang>
2022-02-20 10:51:42 +0700

    page count

--- src/components/Row.js
+++ src/components/Row.js
@@ -6,17 +6,17 @@ const Row = {
     return /* html */ `
       <tr>
         ${column
-          .map((col) => `${Column.print(col.type, data[col.name])}`)
-          .join("")}
+    .map((col) => `${Column.print(col.type, data[col.name])}`)
+    .join("")}
         <td class="w-[100px] px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
           <a href="#/admin/${type}/edit/${
-      data.id
-    }" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+  data.id
+}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
         </td>
         <td class="w-[100px] px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
           <button data-id="${
-            data.id
-          }" id="del_btn" class="text-indigo-600 hover:text-indigo-900">Delete</button>
+  data.id
+}" id="del_btn" class="text-indigo-600 hover:text-indigo-900">Delete</button>
         </td>
       </tr>
     `;
